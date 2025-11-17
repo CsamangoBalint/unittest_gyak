a655782e74924bd0e7c82f88752e1e92dcb71182
Csamangó Bálint <c2csabal@vasvari.org>
2025-11-17 09:12:45 +0100

    main

---
+++ fibo.py
@@ -0,0 +1,20 @@
+import unittest
+
+def fibo(n):
+    if n < 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a,b = 0,1
+    for i in range(2, n+1):
+        a,b = b, a+b
+        return b
+    
+class hasonlit(unittest.TestCase):
+    def test_two_positive(self):
+        fgv = fibo(4)
+        print(fgv)
+    
+unittest.main()
+    
